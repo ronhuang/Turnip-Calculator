ea15ed48e3b571bb0ca94da5340ed074d54fae01
Ron Huang <ron@hng.tw>
2020-04-09 12:08:32 +0800

    MariaDB doesn't support JSON

--- migrations/20200408222937_initial.js
+++ migrations/20200408222937_initial.js
@@ -2,7 +2,7 @@ exports.up = function (knex) {
   return knex.schema.createTable("prices", function (table) {
     table.increments();
     table.string("user_id").notNullable();
-    table.jsonb("values").notNullable();
+    table.text("values").notNullable();
     table.timestamp("created_at").defaultTo(knex.fn.now());
     table.timestamp("updated_at").defaultTo(knex.fn.now());
   });
